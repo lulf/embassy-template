6d4316a8163581135ca257631a8331f0ada7a9ca
Ulf Lilleengen <lulf@redhat.com>
2023-08-31 21:24:38 +0200

    Initial experiment

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+#![no_std]
+#![no_main]
+#![feature(type_alias_impl_trait)]
+
+use embassy_executor::Spawner;
+use embassy_time::{Duration, Timer};
+use {defmt_rtt as _, panic_probe as _};
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    {% if chip contains "nrf" -%}
+    let _p = embassy_nrf::init(Default::default());
+    {% endif -%}
+
+    {% if chip contains "stm32" -%}
+    let _p = embassy_stm32::init(Default::default());
+    {% endif -%}
+
+    {% if chip contains "rp2040" -%}
+    let _p = embassy_rp::init(Default::default());
+    {% endif -%}
+
+    loop {
+        defmt::info!("Blink");
+        Timer::after(Duration::from_millis(100)).await;
+    }
+}
