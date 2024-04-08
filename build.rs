681cd6dd42ec186a4926896ab8bd32aaee8bcec9
Ulf Lilleengen <ulf.lilleengen@gmail.com>
2024-04-08 10:17:03 +0200

    remove defmt env from build script

--- build.rs
+++ build.rs
@@ -32,7 +32,6 @@ fn main() {
     println!("cargo:rustc-link-arg-bins=--nmagic");
     println!("cargo:rustc-link-arg-bins=-Tlink.x");
     println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
-    println!("cargo:rustc-env=DEFMT_LOG=info");
 {% if chip contains "rp2040" -%}
     println!("cargo:rustc-link-arg-bins=-Tlink-rp.x");
 {% endif -%}
