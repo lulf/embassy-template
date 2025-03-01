9528512be20046ec5bc64c21e2e36ff6c882e644
Curly <u007d@humanenginuity.com>
2025-03-01 06:19:24 -0800

    reorder items to make them easier to find; make `main` explicitly divergent, name variable in `main` to help beginners

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,17 @@ use embassy_time::{Duration, Timer};
 use {defmt_rtt as _, panic_probe as _};
 
 #[embassy_executor::main]
-async fn main(_spawner: Spawner) {
+async fn main(_spawner: Spawner) -> ! {
     {% if chip contains "nrf" -%}
-    let _p = embassy_nrf::init(Default::default());
+    let _periphs = embassy_nrf::init(Default::default());
     {% endif -%}
 
     {% if chip contains "stm32" -%}
-    let _p = embassy_stm32::init(Default::default());
+    let _periphs = embassy_stm32::init(Default::default());
     {% endif -%}
 
     {% if chip contains "rp2040" -%}
-    let _p = embassy_rp::init(Default::default());
+    let _periphs = embassy_rp::init(Default::default());
     {% endif -%}
 
     loop {
